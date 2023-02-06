7da33bbe92c3f1d613565386383cead152ca62ff
abdul-kader715 <you@example.com>
2023-02-06 19:06:07 +0600

    tailwidcss

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+  ],
+  theme: {
+    container: {
+    },
+    extend: {
+      spacing: {
+        '5px': '5px',
+      },
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1399px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1199px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '991px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '767px'},
+      // => @media (max-width: 639px) { ... }
+
+      'col': {'max': '567px'},
+      // => @media (max-width: 320px) { ... }
+    },
+    fontFamily: {
+      'base-font': ['Inter', 'sans-serif'],
+      'heading-font': ['Futura PT'],
+    },
+  },
+  plugins: [],
+}
